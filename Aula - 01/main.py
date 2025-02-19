0bdf37a2ab3ca5427f95e85098a442cf8803882c
Fernando S. Axis <159575977+fernandoaxis@users.noreply.github.com>
2025-02-18 22:21:10 -0300

    Add files via upload

---
+++ Aula - 01/main.py
@@ -0,0 +1,17 @@
+#Printa algo na tela
+print("Hello World")
+
+#Variavel A
+a=3
+
+#Estrutura de decisão
+if(a==4):
+    print("A é 4")
+else:
+    print("A não é 4 ")
+
+
+#Função para entrada de valores 
+entrada1 = input("Você está bem?")
+
+print(entrada1)
---
+++ Aula - 01/psicologo.py
@@ -0,0 +1,13 @@
+#Aula 01 - Sistemas Embarcados - Introdução Python - Etec Aristóteles Ferreira
+
+def psicologo():
+
+    entrada1 = input("Você está bem? ")
+
+    if(entrada1 == "Sim"):
+        print("Legal!")
+    else:
+        print("Procure ajuda")
+
+psicologo()
+psicologo()
