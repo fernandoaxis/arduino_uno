8325d5cad94d8e2c303c3813ad82640005bcf73c
Fernando S. Axis <159575977+fernandoaxis@users.noreply.github.com>
2025-02-28 23:14:00 -0300

    Add files via upload

---
+++ Embarcados 25 02 2025 Prof Felippe Borges/sketch_feb28a.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(4, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(4) == LOW)
+  {
+    digitalWrite(13, HIGH);
+    delay(2000);
+    digitalWrite(13, LOW);
+  }
+}
